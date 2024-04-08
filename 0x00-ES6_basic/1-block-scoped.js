cb93db1ba2726eea076ef1ca699f72311f77477c
Josephdada01 <ridwandada500@gmail.com>
2024-04-08 15:22:50 +0000

    fixed the styling

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      const task = true;
-      // disabled  the line
-      const task2 = false;
-      // disabled the line
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    // disabled  the line
+    const task2 = false;
+    // disabled the line
   }
+
+  return [task, task2];
+}
