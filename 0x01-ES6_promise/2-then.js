8f11dcbd0a0ed39b2d39d40bf6c0881d506104de
Josephdada01 <ridwandada500@gmail.com>
2024-04-10 19:22:02 +0000

    using the three handler

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 /* using the the three handlers 'then, response, catch */
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    console.log('Got a response from API');
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }).catch(() => new Error());
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
