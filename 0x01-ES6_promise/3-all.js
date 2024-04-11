412ae2e47737ff5934cc93fa6f71dfac51fb93fe
Josephdada01 <ridwandada500@gmail.com>
2024-04-11 05:37:51 +0000

    going forward

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+/* use the prototype below to collectively resolve all promises
+ * and log body firstName lastName to the console.
+ */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResult, userResult]) => {
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
