855922434bc54ecde88b6c31a9acec76501924a9
Josephdada01 <ridwandada500@gmail.com>
2024-04-11 12:30:36 +0000

    advance task completed

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const result1 = await uploadPhoto();
+    const result2 = await createUser();
+    return {
+      photo: result1,
+      user: result2,
+    };
+  } catch (error) {
+    console.error({ photo: error, user: error });
+    return {};
+  }
+}
