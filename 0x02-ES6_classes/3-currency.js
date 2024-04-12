c250a1388a5bde9c6bf7b794a08cce902793215b
Josephdada01 <ridwandada500@gmail.com>
2024-04-12 10:17:05 +0000

    currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+// Implement a class named Currency
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // getting and setting the code attribute
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+	 throw new TypeError('code must be a string');
+	 }
+	 this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+	  throw new TypeError('name must be a string');
+	  }
+	  this._name = value;
+  }
+
+  // method named displayFullCurrency that will return the attributes in the following format name (code).
+  displayFullCurrency() {
+	   return `${this._name} (${this._code})`;
+	  }
+}
