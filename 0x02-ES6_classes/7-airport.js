58eaeba47b59394594d2c64afe053a92278b8415
Josephdada01 <ridwandada500@gmail.com>
2024-04-12 16:51:58 +0000

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,19 @@
+// Implement a class named Airport
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return this._code;
+  }
+}
