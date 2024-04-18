defcabac8e5d578d62c0297c5bfdd3432926f27c
Josephdada01 <ridwandada500@gmail.com>
2024-04-18 17:16:11 +0000

    adjustment to task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,20 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
     return '';
   }
 
-  let result = '';
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result += (result.length > 0 ? '-' : '') + value.slice(startString.length);
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
   }
-  return result;
+  return parts.join('-');
 }
