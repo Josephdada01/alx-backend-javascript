76d6cfd3b7dca230cfb4761ae4135bef16f32ca7
Josephdada01 <ridwandada500@gmail.com>
2024-04-19 14:50:01 +0100

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,20 +7,50 @@ interface Teacher {
  yearsOfExperience?: number;
  location: string;
  [key: string]: any;
- }
+}
  // this interface extends from the teachers interface
- interface Directors extends Teacher {
+interface Directors extends Teacher {
   numberOfReports: number;
-  }
+}
 
-  function printTeacher(firstName: string, lastName: string): string {
-    return `${firstName.charAt(0)}. {lastName}`
+  
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`
   }
 
   // Define the interface for the function
-  interface printTeacherFunction {
-   (firstName: string, lastName: string): string;
-   }
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+  // Define interface for class constructor
+interface studentClassConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+  // Define interface for class constructor
+interface StudentClassInterface {
+    WorkOnHomework(): string;
+    displayName(): string;
+  }
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  WorkOnHomework(): string {
+    return "Currently working";
+  }
+  
+  displayName(): string {
+    return this.firstName
+  }
+}
 
 
    
