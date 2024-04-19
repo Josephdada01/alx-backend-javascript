f097f3ce87b7aae0b3bdd14608c6b9e24ae70733
Josephdada01 <ridwandada500@gmail.com>
2024-04-19 15:29:52 +0000

    task completed

---
+++ 0x04-TypeScript/task_2/js/main.js
@@ -0,0 +1,63 @@
+// Implement Director class
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return "Working from home";
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return "Getting a coffee break";
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return "Getting to director tasks";
+    };
+    return Director;
+}());
+// Implement Teacher class
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return "Cannot work from home";
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return "Cannot have a break";
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return "Getting to work";
+    };
+    return Teacher;
+}());
+//Define createEmployee function
+function createEmployee(salary) {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
+// Define type predicate function
+function isDirector(employee) {
+    return employee.workDirectorTasks !== undefined;
+}
+// Define executeWork function
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    else {
+        return employee.workTeacherTasks();
+    }
+}
+function teachClass(todayClass) {
+    if (teachClass === "Math") {
+        return "Teaching Math";
+    }
+    else if (teachClass === "History") {
+        return "Teaching History";
+    }
+    else {
+        return "Unknown class";
+    }
+}
---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,81 @@
+// Define DirectorInterface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Define TeacherInterface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Implement Director class
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Implement Teacher class
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+//Define createEmployee function
+function createEmployee(salary: mumber | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+      return new Director();
+  }
+ }
+
+  // Define type predicate function
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+ }
+
+ // Define executeWork function
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+ }
+
+ // Write a String literal type named Subjects allowing a variable to have 
+ //the value Math or History only. Write a function named teachClass:
+
+ type Subjects = "Math" | "History"
+
+ function teachClass(todayClass: Subjects): string {
+   if (teachClass === "Math") {
+     return "Teaching Math";
+   } else if (teachClass === "History") {
+     return "Teaching History";
+   } else {
+      return "Unknown class";
+   }
+ }
