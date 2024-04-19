2c284adae3f0a96480e1663f7097b4e6069ece10
Josephdada01 <ridwandada500@gmail.com>
2024-04-19 16:35:23 +0100

    made a changes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,7 +43,7 @@ class Teacher implements TeacherInterface {
 }
 
 //Define createEmployee function
-function createEmployee(salary: mumber | string): Director | Teacher {
+function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === "number" && salary < 500) {
     return new Teacher();
   } else {
