1b1f8484fd1a89caa708d666466d9ef9e4cfd6e5
Josephdada01 <ridwandada500@gmail.com>
2024-04-19 11:42:21 +0100

    task typscript

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -2,3 +2,33 @@ interface Student {
  firstName: string;
  lastName: string;
  age: number;
+ location: string;
+}
+const student1: Student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    location: "Nigeria",
+};
+
+const student2: Student = {
+    firstName: "Ridwan",
+    lastName: "Dada",
+    age: 50,
+    location: "Ghana",
+};
+
+const studentsList: Student[] = [student1, student2];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const table = document.createElement("table");
+    studentsList.forEach((student) => {
+        const row = table.insertRow();
+        const firstNameCell = row.insertCell(0);
+        const locationCell = row.insertCell(1);
+
+        firstNameCell.textContent = student.firstName;
+        locationCell.textContent = student.location;
+    });
+    document.body.appendChild(table);
+});
