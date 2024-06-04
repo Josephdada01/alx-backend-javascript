5ecda173917b29abc046796da9580d898b16ee81
Josephdada01 <ridwandada500@gmail.com>
2024-06-04 17:48:27 +0100

    Created a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  const { url } = req;
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    const path = './database.csv'; // assume database.csv is in the same directory
+    countStudents(path)
+      .then((result) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write(`This is the list of our students\nNumber of students: ${result.totalStudents}\n`);
+        for (const field in result.students) {
+          if (Object.prototype.hasOwnProperty.call(result.students, field)) {
+            const count = result.students[field].length;
+            const list = result.students[field].join(', ');
+            res.write(`Number of students in ${field}: ${count}. List: ${list}\n`);
+          }
+        }
+        res.end();
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = app;
