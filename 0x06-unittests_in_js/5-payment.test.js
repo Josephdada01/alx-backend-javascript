ad8aa81647d1ab4d3e3bcd10593988db5794b78c
Josephdada01 <ridwandada500@gmail.com>
2024-06-06 15:43:36 +0100

    made adjustment to meet the requirements

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -13,7 +13,7 @@ describe("sendPaymentRequestToApi", function() {
     it("check that console.log is called with the right arg", function() {
         sendPaymentRequestToApi(100, 20);
 
-        expect(console.log.withArgs("The total is: 10").calledOnce).to.be.true;
+        expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
     });
     it("check that console.log is called with the right arg", function() {
         sendPaymentRequestToApi(10, 10);
